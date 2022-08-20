8962038f07dacb84981d2a4d25548228965f0f20
belchanin <belchanin@list.ru>
2022-08-20 18:55:12 +0300

    add json parser

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/belchanin/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/belchanin/frontend-project-46/actions)
+
+### Пример работы программы
+https://asciinema.org/a/GykS3aMTwRfQnUd5RVnOsjSXV
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import fs from "fs";
+import * as path from "path";
+import genDiff from "../src/index.js";
 
 const program = new Command();
 
@@ -10,4 +13,13 @@ program
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => {
+    const file1 = fs.readFileSync(path.resolve(filepath1), 'utf-8');
+    const file2 = fs.readFileSync(path.resolve(filepath2), 'utf-8');
+    const obj1 = JSON.parse(file1);
+    const obj2 = JSON.parse(file2);
+    if (path.extname(filepath1) === '.json') {
+      console.log(genDiff(obj1, obj2));
+    }
+  })
   .parse();
