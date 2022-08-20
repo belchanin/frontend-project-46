46744f4af3ed1ed8b81679be4c2b21a906eb8bbe
belchanin <belchanin@list.ru>
2022-08-20 15:37:03 +0300

    add filepath arguments and format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,6 +7,7 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-
-program.parse();
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .parse();
