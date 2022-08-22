bbd57fd5888facb3e640e9fbf22e4a98375b12d2
belchanin <belchanin@list.ru>
2022-08-22 11:35:37 +0300

    add json format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -58,6 +58,18 @@ Property 'group1.nest' was updated. From [complex value] to 'str'
 Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`;
 
+const expected3 = '[{"name":"common","status":"nested","children":[{"name":"follow","status":"added","value":false},'
+  + '{"name":"setting1","status":"unchanged","value":"Value 1"},{"name":"setting2","status":"deleted","value":200},'
+  + '{"name":"setting3","status":"modified","valueBefore":true,"valueAfter":null},{"name":"setting4","status":"added",'
+  + '"value":"blah blah"},{"name":"setting5","status":"added","value":{"key5":"value5"}},'
+  + '{"name":"setting6","status":"nested","children":[{"name":"doge","status":"nested","children":[{"name":"wow",'
+  + '"status":"modified","valueBefore":"","valueAfter":"so much"}]},{"name":"key","status":"unchanged","value":"value"},'
+  + '{"name":"ops","status":"added","value":"vops"}]}]},{"name":"group1","status":"nested","children":[{"name":"baz",'
+  + '"status":"modified","valueBefore":"bas","valueAfter":"bars"},{"name":"foo","status":"unchanged","value":"bar"},'
+  + '{"name":"nest","status":"modified","valueBefore":{"key":"value"},"valueAfter":"str"}]},{"name":"group2",'
+  + '"status":"deleted","value":{"abc":12345,"deep":{"id":45}}},{"name":"group3","status":"added","value":{"deep":'
+  + '{"id":{"number":45}},"fee":100500}}]';
+
 test('stylish json files', () => {
   const actual = genDiff('__fixtures__/nested1.json', '__fixtures__/nested2.json');
 
@@ -74,4 +86,10 @@ test('plain json files', () => {
   const actual = genDiff('__fixtures__/nested1.yml', '__fixtures__/nested2.yaml', 'plain');
 
   expect(actual).toEqual(expected2);
+});
+
+test('json format', () => {
+  const actual = genDiff('__fixtures__/nested1.yml', '__fixtures__/nested2.yaml', 'json');
+
+  expect(actual).toEqual(expected3);
 });
